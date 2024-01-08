67c6ba3d2fe29468ced413e111f658e0a1fb727e
Siyeong Choi <siychoi@c2r4s6.42seoul.kr>
2024-01-08 14:37:30 +0900

    edit Link

--- src/components/RecommendEvent.js
+++ src/components/RecommendEvent.js
@@ -92,7 +92,7 @@ function RecommendEvent() {
 					</div>
 					<div className="flex justify-center items-center ">
 						<dd className="mt-5 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-							<Link href={"/list"} variant="plain">
+							<Link to={`/list`} variant="plain">
 								<a className="rounded-3xl bg-indigo-600 px-10 py-3 text-base sm:text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">무료 행사 더보기 ></a>
 							</Link>
 						</dd>
